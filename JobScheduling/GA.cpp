780f71b97f08332791eceebd30dce421ba4d1e86
Hao Xu <scyhx2@nottingham.ac.uk>
2020-07-17 20:18:16 +0800

    add glew dependency

--- JobScheduling/GA.cpp
+++ JobScheduling/GA.cpp
@@ -1,4 +1,5 @@
 #include "Population.h"
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <chrono>
 #include <cstdint>
@@ -23,37 +24,7 @@ uint64_t timeSinceEpochMillisec() {
 }
 
 int iteration(int testIndex)
-{
-
-    /*GLFWwindow* window;
-
-    if (!glfwInit())
-        return -1;
-
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0; */
-   
+{   
     Problem prob;
     string filename("TestFile0.txt");
     prob.load_problem(filename, prob);
@@ -128,5 +99,34 @@ int main() {
         time = timeSinceEpochMillisec() - time;
         cout << " Time spent: " << time << " miliseconds" << endl;
     }
+
+    /*
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();*/
     return 0;
 }
