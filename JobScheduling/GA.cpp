9f1e0dd5a8ff399e40b6e2dc41e0ef6db21d85d0
Hao <929153143@qq.com>
2020-07-22 15:43:53 +0800

    lack of keduchi

--- JobScheduling/GA.cpp
+++ JobScheduling/GA.cpp
@@ -8,10 +8,28 @@
 #include <string>
 #include <sstream>
 
+
 using namespace std;
+
+class JobList {
+public:
+    double jobReleasingTime;
+    double jobDeadline;
+    double startTime;
+    double processTime;
+    JobList(double sTime, double pTime, double rTime, double dTime) {
+        jobDeadline = dTime;
+        jobReleasingTime = rTime;
+        startTime = sTime;
+        processTime = pTime;
+    }
+};
+
 Population::Solution bestSolution;
 double bestFitness;
-
+Problem prob;
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 void updateBestSolution(Population pop, Problem prob) {
     for (int i = 0; i < prob.solution_num; i++) {
         if (pop.solutions[i].fitness < bestFitness) {
@@ -28,7 +46,6 @@ uint64_t timeSinceEpochMillisec() {
 
 int iteration(int testIndex)
 {   
-    Problem prob;
     string filename("TestFile0.txt");
     prob.load_problem(filename, prob);
     prob.solution_num = 10;
@@ -95,6 +112,7 @@ int iteration(int testIndex)
     return 0;
 }
 
+/*
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
@@ -164,16 +182,8 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
     glDeleteShader(fs);
 
     return program;
-}
-
-int main() {
-    for (int i = 0; i < 1; i++) {
-        uint64_t time = timeSinceEpochMillisec();
-        iteration(i);
-        time = timeSinceEpochMillisec() - time;
-        cout << " Time spent: " << time << " miliseconds" << endl;
-    }
-
+}*/
+int draw() {
     GLFWwindow* window;
 
     if (!glfwInit())
@@ -194,88 +204,126 @@ int main() {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    ifstream in("TestFile0.txt");
-    int job_num = 0;
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glMatrixMode(GL_PROJECTION); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+    glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); // essentially set coordinate system
+    glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+    glLoadIdentity(); // same as above comment
+
     string line;
     string str1, str2, str3;
-    float height = 0.8f;
-    while (getline(in, line))
-    {
-        job_num++;
-    }
-    in.clear();
-    in.seekg(0, ios::beg);
-    float* positions = new float[job_num * 8];
-    in.seekg(0, fstream::beg);
+
+    float height = 0.9 * SCREEN_HEIGHT;
+
+    float segment = SCREEN_HEIGHT / (prob.job_num + 1);
+    GLfloat* lineVertices = new GLfloat[prob.job_num * 6];
+    GLfloat* redLineVertices = new GLfloat[prob.job_num * 6];
     int position_index = 0;
     int vertex_num = 0;
 
-    while (getline(in, line))
-    {
-        stringstream word(line);
-        word >> str1;
-        word >> str2;
-        word >> str3;
-
-        float releaseTime = atof(str1.c_str()); 
-        float deadLine = atof(str2.c_str());
-        float processTime = atof(str3.c_str());
-
-        positions[position_index] = releaseTime/20-1.0;    
-        positions[position_index + 1] = height;            
-        positions[position_index + 2] = deadLine/20-1.0;   
-        positions[position_index + 3] = height;            
-        positions[position_index + 4] = deadLine/20-1.0;   
-        positions[position_index + 5] = height-0.01;       
-        positions[position_index + 6] = releaseTime/20-1.0;
-        positions[position_index + 7] = height-0.01;       
-        position_index += 8;
-        height -= 0.08;
+    vector<JobList> bestJobList;
+    for (int i = 0; i < bestSolution.jobStartTimes.size(); i++) {
+        JobList* curJob = new JobList(bestSolution.jobStartTimes[i], prob.jobs[i].processTime, prob.jobs[i].releaseTime, prob.jobs[i].deadLine);
+        bestJobList.push_back(*curJob);
+        //   cout << jobs[i].startTime << " " << jobs[i].processTime << endl;
     }
 
-    unsigned int* indices = new unsigned int[job_num*6];
-    int indices_index = 0;
-    int job_index = 0;
-    while (job_index < job_num) {
-        indices[indices_index] = 0+ job_index*4;    
-        indices[indices_index+1] = 1 + job_index*4; 
-        indices[indices_index+2] = 2 + job_index*4; 
-        indices[indices_index+3] = 2 + job_index*4; 
-        indices[indices_index+4] = 3 + job_index*4; 
-        indices[indices_index+5] = 0 + job_index*4; 
-        indices_index += 6;
-        job_index++;
+    std::sort(bestJobList.begin(), bestJobList.end(),
+        [](const JobList& a, const JobList& b)
+        {
+            return a.jobReleasingTime <= b.jobReleasingTime;
+            
+        }
+    );
+
+    for (int i = 0; i < prob.job_num; i++) {
+        lineVertices[position_index] = bestJobList[i].jobReleasingTime * 30;
+        lineVertices[position_index + 1] = height;
+        lineVertices[position_index + 2] = 0;
+        lineVertices[position_index + 3] = bestJobList[i].jobDeadline * 30;
+        lineVertices[position_index + 4] = height;
+        lineVertices[position_index + 5] = 0;
+        position_index += 6;
+        height -= segment;
     }
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 4 * job_num * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    position_index = 0;
+    height = 0.9 * SCREEN_HEIGHT;
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    for (int i = 0; i < prob.job_num; i++) {
+        redLineVertices[position_index] = bestJobList[i].startTime * 30;
+        redLineVertices[position_index + 1] = height;
+        redLineVertices[position_index + 2] = 0;
+        redLineVertices[position_index + 3] = (bestJobList[i].startTime + bestJobList[i].processTime)* 30;
+        redLineVertices[position_index + 4] = height;
+        redLineVertices[position_index + 5] = 0;
+        position_index += 6;
+        height -= segment;
+    }
+    /*
+        while (getline(in, line))
+        {
+            stringstream word(line);
+            word >> str1;
+            word >> str2;
+            word >> str3;
 
-    unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, job_num * 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+            float releaseTime = atof(str1.c_str());
+            float deadLine = atof(str2.c_str());
+            float processTime = atof(str3.c_str());
+            lineVertices[position_index] = releaseTime * 30;
+            lineVertices[position_index + 1] = height;
+            lineVertices[position_index + 2] = 0;
+            lineVertices[position_index + 3] = deadLine*30;
+            lineVertices[position_index + 4] = height;
+            lineVertices[position_index + 5] = 0;
+            position_index += 6;
+            height -= segment;
+        }
+    */
 
 
-    ShaderProgramSource source = ParseShader("Basic.shader");
 
-    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
-
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
-     
-        glDrawElements(GL_TRIANGLES, job_num * 6, GL_UNSIGNED_INT, nullptr);
+
+        // Render OpenGL here
+        glEnable(GL_LINE_SMOOTH);
+        glEnable(GL_LINE_STIPPLE);
+        glPushAttrib(GL_LINE_BIT);
+        glLineWidth(4);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glColor3f(1, 1, 1);
+        glVertexPointer(3, GL_FLOAT, 0, lineVertices);
+        glDrawArrays(GL_LINES, 0, 2 * prob.job_num);
+        glColor3f(1, 0, 0);
+        glVertexPointer(3, GL_FLOAT, 0, redLineVertices);
+        glDrawArrays(GL_LINES, 0, 2 * prob.job_num);
+
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+
+        // Swap front and back buffers
         glfwSwapBuffers(window);
+
+        // Poll for and process events
         glfwPollEvents();
     }
 
-    glDeleteProgram(shader);
-
     glfwTerminate();
+
+}
+
+int main() {
+    for (int i = 0; i < 1; i++) {
+        uint64_t time = timeSinceEpochMillisec();
+        iteration(i);
+        time = timeSinceEpochMillisec() - time;
+        cout << " Time spent: " << time << " miliseconds" << endl;
+    }
+    draw();
     return 0;
 }
+
